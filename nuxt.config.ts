be61b9982e2fe7afad05bbd786fae0e972b1c279
Vicky Chan <s6102161021@gamil.com>
2026-05-10 22:38:36 +0800

    測試 CI 與 Github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  app: {
+    // 如果不是開發環境 (即生產環境)，就使用 /veekend-nuxt/
+    baseURL: process.env.NODE_ENV === "production" ? "/veekend-nuxt/" : "/",
+  },
+
   devServer: {
     host: "127.0.0.1",
     port: 9999,
