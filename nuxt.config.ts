4f8d7ab0e157c6bbdd3a6c53429b767cc751defe
Vicky Chan <s6102161021@gmail.com>
2026-05-11 10:08:47 +0800

    code 風格設定與 SCSS 安裝設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   app: {
     // 如果不是開發環境 (即生產環境)，就使用 /veekend-nuxt/
-    baseURL: process.env.NODE_ENV === "production" ? "/veekend-nuxt/" : "/",
+    baseURL: process.env.NODE_ENV === 'production' ? '/veekend-nuxt/' : '/',
   },
   devServer: {
-    host: "127.0.0.1",
+    host: '127.0.0.1',
     port: 9999,
   },
+  css: ['@/assets/scss/main.scss'],
   vite: {
     optimizeDeps: {
-      include: ["@vue/devtools-core", "@vue/devtools-kit"],
+      include: ['@vue/devtools-core', '@vue/devtools-kit'],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/preprocess.scss" as *;',
+        },
+      },
     },
   },
-  modules: [
-    "@nuxt/a11y",
-    "@nuxt/content",
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-  ],
-});
+  modules: ['@nuxt/a11y', '@nuxt/content', '@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+})
